ab8b7bc7e4f24afd8b9fe153594d3e8f5afdf1a1
denisebartolome <dpbartolome@gmail.com>
2014-09-20 14:52:13 -0700

    added initial files

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+$('a').on('click', function(e){
+    e.preventDefault();
+    var targetID = $(this).attr('href');
+    var elementPosition = $(targetID).offset().top;
+    $('html,body').animate({scrollTop: elementPosition},'slow');
+});
+  
+});
